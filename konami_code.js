b0c2598f17489e84847927a06844a56c785ff973
Jasmine Lok <jasminellok@gmail.com>
2020-05-06 01:39:38 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -17,5 +17,16 @@ function init() {
   document.body.addEventListener("keydown", (event) => {
     console.log(event.key);
   }
+  let index = 0;
+
+  if (codes.indexOf(event.key) < 0 || event.key !== codes[index]) {
+		index = 0;
+		return;
+	} else {
+    index++
+    if (pattern.length === current) {
+		current = 0;
+		window.alert('You found it!');
+  }
   
 }
