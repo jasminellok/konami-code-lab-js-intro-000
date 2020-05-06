60b975494cfcb478c49c136f0b764d95fba8fac5
Jasmine Lok <jasminellok@gmail.com>
2020-05-06 01:31:08 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -14,22 +14,8 @@ const codes = [
 var index = 0;
 
 function init() {
-  var index = 0
-  document.body.addEventListener('keydown', event);
-
-  function event(e) {
-    const key = parseInt(e.detail || e.which)
-    console.log('key', key)
-    if (key === code[index]) {
-      index++
-
-      if (index === code.length) {
-        alert("Hurray!")
-        index = 0
-      }
-    } else {
-      index = 0
-    }
+  document.body.addEventListener("keydown", (event) => {
+    console.log(event.key);
   }
-
+  
 }
